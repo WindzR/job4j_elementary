bcecdc70f8964f1f229d58c5bc8dcf961b9cc3d1
WindzoR <windzr67@gmail.com>
2020-10-18 17:19:50 +0300

    9.1. String [#389867]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String newbie = "But I am a newbie.";
+        idea = idea + newbie;
+        int year = 2020;
+        idea = idea + year;
+        System.out.println(idea);
+    }
+}
