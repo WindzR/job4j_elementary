d72be397881992b3781088dce79ff3847d2cd5a2
WindzoR <windzr67@gmail.com>
2020-10-20 22:44:20 +0300

    4.3.6. Math.abs [#389816]

---
+++ src/ru/job4j/condition/ChessBoard.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class ChessBoard {
+
+    public static int way(int x1, int y1, int x2, int y2) {
+        if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+            return Math.abs(x2 - x1);
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Слон переместился на: " + way(0, 0, 5, 5) + " ходов.");
+        System.out.println("Слон переместился на: " + way(0, 0, 3, 5) + " ходов.");
+        System.out.println("Слон переместился на: " + way(7, 0, 0, 7) + " ходов.");
+        System.out.println("Слон переместился на: " + way(2, 1, 7, 7) + " ходов.");
+    }
+
+
+}
