f083defeabbd91814556f3ec6cee6fe19971b7b1
WindzoR <windzr67@gmail.com>
2020-12-15 15:02:05 +0300

    4. final [#390006]

---
+++ src/ru/job4j/ooa/AirCraft.java
@@ -0,0 +1,9 @@
+package ru.job4j.ooa;
+
+public class AirCraft {
+    private final String MODEL_NAME = "Метод выводит в консоль модель самолета.";
+
+    public void printModel() {
+        System.out.println(MODEL_NAME);
+    }
+}
