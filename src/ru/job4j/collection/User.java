c945a0146b504e6e2674ad1f0aaad91ea5a628a1
WindzoR <windzr67@gmail.com>
2021-01-08 14:00:00 +0300

    1. Организовать сортировку User [#389941]

---
+++ src/ru/job4j/collection/User.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User another) {
+        int compare = this.name.compareTo(another.name);
+        if (compare == 0) {
+            return Integer.compare(age, another.age);
+        }
+        return compare;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
