6f6a53d8e004895a62ba2d21a58a382c2f7a1aad
WindzoR <windzr67@gmail.com>
2021-01-13 20:19:17 +0300

    3. Что такое JavaDoc [#389932]

--- src/ru/job4j/bank/Account.java
+++ src/ru/job4j/bank/Account.java
@@ -2,31 +2,67 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * класс Account описывает модель данных аккаунта(счета) пользователя user
+ * @author Bodin Dmitry
+ * @version 1.0
+ */
 public class Account {
+    /**
+     * Хранение данных осуществляется
+     * String requisite реквизиты счета
+     * double balance баланс счета
+     */
     private String requisite;
     private double balance;
 
+    /**
+     * конструктор объекта Account
+     * @param requisite инициализация реквизитов счета в памяти
+     * @param balance инициализация баланса счета в памяти
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * геттер поля requisite
+     * @return requisite, номер счета объекта Account
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * сеттер поля requisite
+     * @param requisite , позволяет переписать данные счета Account
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * геттер поля balance
+     * @return balance, баланс счета объекта Account
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * сеттер поля requisite
+     * @param balance , позволяет изменять(списывать или начисления) счета Account
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * переопреление equals для ACCOUNT
+     * @param o переопределяем o и приводим его к объекту ACCOUNT
+     * @return requisite, переопределяем equals только для поля requisite
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -35,6 +71,10 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * переопределение hashcode для ACCOUNT
+     * @return hashcode для поля requisite
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
@@ -42,9 +82,9 @@ public class Account {
 
     @Override
     public String toString() {
-        return "Account{" +
-                "requisite='" + requisite + '\'' +
-                ", balance=" + balance +
-                '}';
+        return "Account{"
+                + "requisite='" + requisite + '\''
+                + ", balance=" + balance
+                + '}';
     }
 }
