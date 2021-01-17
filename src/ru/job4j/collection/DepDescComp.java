9318f98b8934370921ad4dc85d8286812ad6e6cb
WindzoR <windzr67@gmail.com>
2021-01-17 13:46:31 +0300

    2. Отсортировать департаменты [#389935]

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -8,13 +8,11 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String first, String second) {
         String[] firstLex = first.split("/");
         String[] secondLex = second.split("/");
-        int rslLength = Math.min(firstLex.length, secondLex.length);
-        for (int i = 0; i < rslLength; i++) {
-            int rsl = firstLex[i].compareTo(secondLex[i]);
-            if (rsl != 0) {
-                return rsl;
-            }
+        int rsl = secondLex[0].compareTo(firstLex[0]);
+        if (rsl == 0) {
+            return first.compareTo(second);
+        } else {
+            return rsl;
         }
-        return Integer.compare(firstLex.length, secondLex.length);
     }
 }
