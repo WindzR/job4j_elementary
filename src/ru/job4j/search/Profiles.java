af0cf3f951dad5431e06122ff1b0efda940f9cd9
WindzoR <windzr67@gmail.com>
2021-01-25 14:09:37 +0300

    3. Уникальность элементов и сортировка. [#389780]

--- src/ru/job4j/search/Profiles.java
+++ src/ru/job4j/search/Profiles.java
@@ -1,6 +1,8 @@
 package ru.job4j.search;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class Profiles {
@@ -9,6 +11,16 @@ public class Profiles {
         return profiles
                 .stream()
                 .map(Profile::getAddress)
+                .sorted(new AddressComparator())
+                .distinct()
                 .collect(Collectors.toList());
+    }
+
+    private class AddressComparator implements Comparator<Address> {
+
+        @Override
+        public int compare(Address first, Address second) {
+            return first.getCity().toUpperCase().compareTo(second.getCity().toUpperCase());
+        }
     }
 }
--- test/ru/job4j/search/ProfilesTest.java
+++ test/ru/job4j/search/ProfilesTest.java
@@ -24,4 +24,23 @@ public class ProfilesTest {
         );
         assertThat(actual, is(expected));
     }
+
+    @Test
+    public void sortedListOfAddress() {
+        Profiles profileTest = new Profiles();
+        List<Address> actual = profileTest.collect(List.of(
+                new Profile(new Address("Moscow", "Lenina", 220150, 60)),
+                new Profile(new Address("Peterburg", "Gagarina", 666666, 13)),
+                new Profile(new Address("Bryansk", "Marksa", 100500, 102)),
+                new Profile(new Address("Tula", "Jukova", 520230, 444)),
+                new Profile(new Address("Peterburg", "Gagarina", 666666, 13))
+        ));
+        List<Address> expected = List.of(
+                new Address("Bryansk", "Marksa", 100500, 102),
+                new Address("Moscow", "Lenina", 220150, 60),
+                new Address("Peterburg", "Gagarina", 666666, 13),
+                new Address("Tula", "Jukova", 520230, 444)
+        );
+        assertThat(actual, is(expected));
+    }
 }
