91d051fa84d08d639ba17416ed77f9c3cfed05cf
WindzoR <windzr67@gmail.com>
2021-01-31 14:32:56 +0300

    1. Метод .of [#389789]

--- test/ru/job4j/collection/NotifyAccountTest.java
+++ test/ru/job4j/collection/NotifyAccountTest.java
@@ -13,12 +13,12 @@ public class NotifyAccountTest {
 
     @Test
     public void sent() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        HashSet<Account> expect = new HashSet(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -28,14 +28,14 @@ public class NotifyAccountTest {
 
     @Test
     public void sentDuplicate() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("142", "Petr Arsentev", "000001"),
                 new Account("123", "Petr Arsentev", "eDer3432f")
         );
-        HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+        HashSet<Account> expect = new HashSet(
+                List.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
